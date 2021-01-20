e5fc006621014cafb649578d97289f8cdd906a8e
Drew Thompson <drewallynthompson@gmail.com>
2021-01-20 07:27:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your classes here
+class Tree {
+  constructor(species){
+    this.species = species;
+  }
+  
+  static definition(){
+    return 'A tree is a perenial plant with an elongated stem, or trunk supporting branches and leaves.'
+  }
+}
